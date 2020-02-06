d4cf2d0975f9e5d8c5c6ccb93502d26ea64e65a6
Dang Tu Anh <58834452+tuanh610@users.noreply.github.com>
2020-02-06 08:07:09 +0800

    Fix edit issue where instead of edit, it just add a new one

--- RequestManager/models.py
+++ RequestManager/models.py
@@ -33,3 +33,5 @@ class Request(models.Model):
 
     def __str__(self):
         return 'Priority {} from {}'.format(self.client_priority, self.client.client_name)
+
+
--- RequestManager/views.py
+++ RequestManager/views.py
@@ -23,13 +23,15 @@ def edit_request(request):
             request_id = request.POST["id"]
             item = Request.objects.get(pk=request_id)
             request_form = RequestForm(instance=item)
-            return render(request, template_name='RequestManager/new_request.html', context={"form": request_form})
+            return render(request, template_name='RequestManager/edit_request.html',
+                          context={"form": request_form, "request_id": request_id})
         except ObjectDoesNotExist:
             return render(request, template_name='home.html',
                           context={"error_message": "Request ID incorrect. Cannot find request"})
     else:
         return render(request, template_name='home.html',
                       context={"error_message": "Invalid request"})
+
 
 def delete_request(request):
     if request.method == "POST":
@@ -59,27 +61,14 @@ def request_result(request):
         if request.method == "POST":
             if "cancel" in request.POST:
                 return redirect('RequestManager:home')
-            requestform = RequestForm(request.POST)
-            if requestform.is_valid():
-                item = requestform.save()
-                client_detail = ClientDetail.objects.get(pk=item.client.id)
-                client_detail.request_amount += 1
-                client_detail.save()
-                current_priority = item.client_priority
-                current_id = item.id
+            if "request_id" in request.POST:
+                olditem = Request.objects.get(pk=request.POST["request_id"])
+                requestform = RequestForm(request.POST, instance=olditem)
+            else:
+                requestform = RequestForm(request.POST)
 
-                while True:
-                    update_required = Request.objects.filter(client__client_name=item.client.client_name).filter(
-                        client_priority=current_priority).exclude(id=current_id)
-                    if len(update_required) == 0:
-                        break
-                    elif len(update_required) == 1:
-                        update_required[0].client_priority += 1
-                        update_required[0].save()
-                        current_priority += 1
-                        current_id = update_required[0].id
-                    else:
-                        raise Exception("Request data has been tampered. Please use admin to correct it")
+            if requestform.is_valid():
+                item = updateDB(requestform)
             else:
                 return render(request, 'RequestManager/request_result.html',
                               {'error': "Data Input Incorrect. Please re-check"})
@@ -90,6 +79,27 @@ def request_result(request):
     except Exception as e:
         return render(request, 'RequestManager/request_result.html', {'error': str(e)})
 
+def updateDB(requestform):
+    item = requestform.save()
+    client_detail = ClientDetail.objects.get(pk=item.client.id)
+    client_detail.request_amount += 1
+    client_detail.save()
+    current_priority = item.client_priority
+    current_id = item.id
+
+    while True:
+        update_required = Request.objects.filter(client__client_name=item.client.client_name).filter(
+            client_priority=current_priority).exclude(id=current_id)
+        if len(update_required) == 0:
+            break
+        elif len(update_required) == 1:
+            update_required[0].client_priority += 1
+            update_required[0].save()
+            current_priority += 1
+            current_id = update_required[0].id
+        else:
+            raise Exception("Request data has been tampered. Please use admin to correct it")
+    return item
 
 def all_requests(request, page):
     all_requests = list(Request.objects.all())
